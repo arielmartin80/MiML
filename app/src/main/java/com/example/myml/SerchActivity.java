ce5b8eb0b06c131966677e6c4fda5dc2a424abb7
Ariel Martin <arielmartin80@gmail.com>
2019-06-21 11:12:15 -0300

    librerias

--- app/src/main/java/com/example/myml/SerchActivity.java
+++ app/src/main/java/com/example/myml/SerchActivity.java
@@ -5,11 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import com.squareup.picasso.Picasso;
 
 
 public class SerchActivity extends AppCompatActivity {
 
     private EditText et1;
+
+    @BindView(R.id.imageViewML)
+    ImageView imageViewML;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,9 @@ public class SerchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         et1 = (EditText)findViewById(R.id.et_buscar);
+
+       // Picasso.with(this).load("https://seeklogo.com/images/M/mercado-libre-logo-15D357DA7B-seeklogo.com.png”).into(imageViewML);
+
     }
 
     //Metodo para el boton buscar
